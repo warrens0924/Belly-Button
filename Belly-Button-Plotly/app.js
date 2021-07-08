d0d0c6a05ab5c6ce47b5dcf7ada3b2abca29baf7
Warren <warrens0924@gmail.com>
2021-07-07 20:39:36 -0700

    new

--- Belly-Button-Plotly/app.js
+++ Belly-Button-Plotly/app.js
@@ -1,34 +1,27 @@
-function init() {
+
+function getData(sampleID){
+    d3.json("samples.json").then((importedData) => {
+        var data = importedData;
+        console.log(data);
+
+        var sampleData = data.samples;
+        console.log(sampleValue);
+        var resultArr = sampleData.filter(sampleObject => sampleObject.id == sampleID);
+        var variableArr = resultArr[0];
+        console.log(variableArr);
+        var sampleValue = variableArr.sample_values;
+        //console.log(sampleValue);
+        var otuID = variableArr.otu_ids;
+        //console.log(otuID);
+        var otuLabel = variableArr.otu_labels;
+        //console.log(otuLabel);
+
+        //get wfreqdata
+        var mData = data.metadata;
+        var resultFreq = mData.filter(freqObject => freqObject.id == sampleID);
+        var freqData = resultFreq[0];
+        console.log(freqData);
+        var wFreq = freqData.wfreq;
     
-    // add the ids to the drop down menu
-    var id = d3.select('#selDataset');
-    d3.json('data/samples.json').then((data) => {
-        var sampleNames = data.names;
-        sampleNames.forEach((name)=> {
-            id.append('option').text(name).property('value', name)
-        });
-    });
-
-
-};
-
-init();
-// D3.selectAll("#selDataset").on("change", getData);
-
-
-// function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-//     // Initialize an empty array for the country's data
-//     var data = [];
-  
-//     if (dataset == 'us') {
-//         data = us;
-//     }
-//     else if (dataset == 'uk') {
-//         data = uk;
-//     }
-//     else if (dataset == 'canada') {
-//         data = canada;
-//     }
+    })
+}
